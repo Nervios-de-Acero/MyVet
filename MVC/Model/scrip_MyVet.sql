b3c7914d8416aaf37fddee76222eb7523591ca38
Yanina-Unrein <unrein.syanina@gmail.com>
2022-11-11 00:52:16 -0300

    Tablas intermedias Asigna y Trabajan BD

--- MVC/Model/scrip_MyVet.sql
+++ MVC/Model/scrip_MyVet.sql
@@ -33,6 +33,17 @@ CREATE TABLE animal (
 	Esterilizado boolean NOT NULL
 );
 
+ ALTER TABLE animal DROP COLUMN Vacuna; 
+  ALTER TABLE animal DROP COLUMN Id_Vacuna; 
+  ALTER TABLE animal DROP COLUMN Desparasitacion;
+  ALTER TABLE animal DROP COLUMN Turno;
+  ALTER TABLE animal DROP COLUMN Diagnostico;
+  
+  ALTER TABLE animal MODIFY Edad varchar(30);
+  ALTER TABLE animal ADD Especie varchar(15) AFTER Edad;
+  ALTER TABLE animal ADD Características varchar(200) AFTER Esterilizado;
+
+
 CREATE TABLE Sucursales 
 (
 Id_Sucursal int PRIMARY KEY not null auto_increment, 
@@ -55,6 +66,29 @@ CREATE TABLE visita (
   observaciones varchar(250)
   );
 
+   CREATE TABLE Turno 
+  (
+  id_turno INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  fecha_turno DATE NOT NULL,
+  especialidad VARCHAR(30) NOT NULL
+  );
+
+   ALTER TABLE Turno
+   RENAME COLUMN id_turno TO ID_turno;  
+
+ CREATE TABLE Profesional
+  (
+  ID_Profesional INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  Nombre VARCHAR(100) NOT NULL,
+  Especialidad VARCHAR(30) NOT NULL
+  );
+
+  SELECT * FROM Profesional;
+
+  ALTER TABLE Profesional
+  MODIFY Especialidad VARCHAR(100);
+
+
   CREATE TABLE Solicita (
   ID_Turno1 int NOT NULL,
   ID_Mascota4 int NOT NULL,
@@ -68,7 +102,36 @@ CREATE TABLE visita (
   foreign key(ID_Mascota5) references animal(ID_Mascota),
   foreign key(ID_Profesional4) references Profesional(ID_Profesional)
   );
+
+  CREATE TABLE Protege
+  (
+  id_Cliente1 INT NOT NULL,
+  id_Mascota1 INT NOT NULL,
+  FOREIGN KEY(id_Cliente1) REFERENCES usuario(Id_Cliente),
+  FOREIGN KEY(id_Mascota1) REFERENCES animal(ID_Mascota)
+  );
+
+  ALTER TABLE Protege
+  RENAME COLUMN id_Cliente1 TO ID_Cliente1,
+  RENAME COLUMN id_Mascota1 TO ID_Mascota1; 
+
+  CREATE TABLE Asigna 
+  (
+	ID_Turno2 INT NOT NULL,
+    ID_Profesional2 INT NOT NULL,
+    FOREIGN KEY(ID_Turno2) REFERENCES Turno(id_turno),
+    FOREIGN KEY(ID_Profesional2) REFERENCES Profesional(ID_Profesional)
+  );
   
+  CREATE TABLE Trabajan
+  (
+	ID_Sucursal1 INT NOT NULL,
+	ID_Profesional1 INT NOT NULL,
+    FOREIGN KEY(ID_Sucursal1) REFERENCES Sucursales(Id_Sucursal),
+    FOREIGN KEY(ID_Profesional1) REFERENCES Profesional(ID_Profesional)
+  );
+
+
   select * from Tratamiento;
   
   Insert into Tratamiento (nombre_tratamiento, fecha, tipo, observaciones)
@@ -80,16 +143,7 @@ CREATE TABLE visita (
 
   SELECT * FROM animal;
   
-  ALTER TABLE animal DROP COLUMN Vacuna; 
-  ALTER TABLE animal DROP COLUMN Id_Vacuna; 
-  ALTER TABLE animal DROP COLUMN Desparasitacion;
-  ALTER TABLE animal DROP COLUMN Turno;
-  ALTER TABLE animal DROP COLUMN Diagnostico;
-  
-  ALTER TABLE animal MODIFY Edad varchar(30);
-  ALTER TABLE animal ADD Especie varchar(15) AFTER Edad;
-  ALTER TABLE animal ADD Características varchar(200) AFTER Esterilizado;
-
+ 
   INSERT INTO animal (Nombre, Edad, Especie, Raza,Peso,Sexo,Fecha_nac,Esterilizado, Características)
   VALUES ("Firulais","5 años", "Perro", "Pomerania",2,"Masculino","2017-08-05",True, "Tostado");
   
@@ -99,43 +153,10 @@ CREATE TABLE visita (
   UPDATE animal SET Nombre = "Azirafel", Edad = "2 años", Raza = "Común europeo", Peso = 4, Fecha_nac = "2020-07-30", Características = "Rubio"  
   WHERE ID_Mascota = 3;
   
-  CREATE TABLE Turno 
-  (
-  id_turno INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-  fecha_turno DATE NOT NULL,
-  especialidad VARCHAR(30) NOT NULL
-  );
-
-  CREATE TABLE Protege
-  (
-  id_Cliente1 INT NOT NULL,
-  id_Mascota1 INT NOT NULL,
-  FOREIGN KEY(id_Cliente1) REFERENCES usuario(Id_Cliente),
-  FOREIGN KEY(id_Mascota1) REFERENCES animal(ID_Mascota)
-  );
-
-  ALTER TABLE Turno
-  RENAME COLUMN id_turno TO ID_turno;  
-
-  ALTER TABLE Protege
-  RENAME COLUMN id_Cliente1 TO ID_Cliente1,
-  RENAME COLUMN id_Mascota1 TO ID_Mascota1; 
-
+  
   SELECT * FROM Protege;
 
   SELECT * FROM Turno;
-
-  CREATE TABLE Profesional
-  (
-  ID_Profesional INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  Nombre VARCHAR(100) NOT NULL,
-  Especialidad VARCHAR(30) NOT NULL
-  );
-
-  SELECT * FROM Profesional;
-
-  ALTER TABLE Profesional
-  MODIFY Especialidad VARCHAR(100);
 
   INSERT INTO Profesional(Nombre, Especialidad)
   VALUES("María Alejandra Del Pilar Ugarte Laclós", "Dermatología");
@@ -143,9 +164,6 @@ CREATE TABLE visita (
   INSERT INTO Profesional(Nombre, Especialidad)
   VALUES("Antonio Milisenda", "Diagnóstico por imágenes orientación radiología"),
       ("Carolina Altamirano", "Clínica médica");
-
-
-
 
 
  /* No hay conexión entre tablas aun-
@@ -170,3 +188,6 @@ VALUES ("Aylen", "Gil", 38644396, "lag9512@gmail.com", "Calle Falsa 123", 112233
 SELECT * FROM usuario;
 
 SELECT * FROM Protege;
+
+INSERT INTO animal (Nombre, Edad, Especie, Raza,Peso,Sexo,Fecha_nac,Esterilizado, Características)
+VALUES ("Bills","5 años", "Gato", "Común Europeo",6,"Masculino","2017-03-30",True, "Blanco con manchas atigradas marrones y negras");
