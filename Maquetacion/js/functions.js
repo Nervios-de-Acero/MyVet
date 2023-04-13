05e1e02470346087bba2892d805a443edac4b8eb
Yanina <unrein.syanina@gamil.com>
2023-04-13 20:56:16 -0300

    acomodando la estructura del proyecto

---
+++ Maquetacion/js/functions.js
@@ -0,0 +1,8 @@
+function redirigirBoton(boton){
+    if (boton === "login"){
+      window.location.href = "inicioSesion.html"
+
+    }else if (boton === "register"){
+      window.location.href="registro.html"
+    }
+  }
