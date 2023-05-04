796f5eca398010c08124450aa6eb33e4be2f8a23
Lorena Aylen <lag9512@gmail.com>
2023-05-03 23:43:13 -0300

    Cambié el nombre de algunas carpetas, realicé la importación del módulo Servicios.

--- Frontend/myvet.app/src/app/pages/pages.module.ts
+++ Frontend/myvet.app/src/app/pages/pages.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ServiciosComponent } from './servicios/servicios.component';
+import { ServiciosComponent } from './ServiciosComponent/servicios.component';
 
 
 
