74dcc93d66b2478f5e1d8f9a17f740a3c63642ff
Emilia <emirod467@gmail.com>
2023-05-03 22:56:27 -0300

    agregué componente Inicio

--- Frontend/myvet.app/src/app/pages/pages.module.ts
+++ Frontend/myvet.app/src/app/pages/pages.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { InicioComponent } from './inicio/inicio.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    InicioComponent
+  ],
   imports: [
     CommonModule
   ]
