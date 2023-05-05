3ec17e5c2ac63d1ac3b6b6cc83714c923649fbc4
Emilia <emirod467@gmail.com>
2023-05-05 20:53:56 -0300

    Cree componente Header

---
+++ Frontend/myvet.app/src/app/shared/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+
+}
