d3dbb5713b4fc207b7bef9301e1cdee6b7d7b4da
Emilia <emirod467@gmail.com>
2023-05-05 10:44:13 -0300

    Agregué módulo ecommerce a Develop

--- Frontend/myvet.app/src/app/ecommerce/ecommerce.module.ts
+++ Frontend/myvet.app/src/app/ecommerce/ecommerce.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+//subo este comment para hacer un nuevo push, luego lo borramos//
 
 @NgModule({
   declarations: [],
