24f61e0f0d3d6bed40996ff65701e3df003b383b
Uriel <urield1917@gmail.com>
2023-05-07 17:36:18 -0300

    Componente Categorias creado

--- Frontend/myvet.app/src/app/ecommerce/ecommerce.module.ts
+++ Frontend/myvet.app/src/app/ecommerce/ecommerce.module.ts
@@ -1,20 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+<<<<<<< Updated upstream
 import { CatalogoComponent } from './catalogo/catalogo.component';
 import { ProductoComponent } from './producto/producto.component';
 import { BannerComponent } from './banner/banner.component';
 import { VistaDetalladaComponent } from './vista-detallada/vista-detallada.component';
 import { RouterModule } from '@angular/router';
 import { BuscarAnimalesComponent } from './buscar-animales/buscar-animales.component';
+=======
+import { CategoriasComponent } from './categorias/categorias.component';
+>>>>>>> Stashed changes
 
 
 @NgModule({
   declarations: [
+<<<<<<< Updated upstream
     CatalogoComponent,
     ProductoComponent,
     BannerComponent,
     VistaDetalladaComponent,
     BuscarAnimalesComponent
+=======
+    CategoriasComponent
+>>>>>>> Stashed changes
   ],
   imports: [
     CommonModule
