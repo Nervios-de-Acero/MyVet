7acc63ce44a7aa6598aca9454f0edaee7fa9b64c
Uriel <urield1917@gmail.com>
2023-05-07 17:44:21 -0300

    Corregí los cambios solicitados

--- Frontend/myvet.app/src/app/ecommerce/ecommerce.module.ts
+++ Frontend/myvet.app/src/app/ecommerce/ecommerce.module.ts
@@ -1,28 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-<<<<<<< Updated upstream
 import { CatalogoComponent } from './catalogo/catalogo.component';
 import { ProductoComponent } from './producto/producto.component';
 import { BannerComponent } from './banner/banner.component';
 import { VistaDetalladaComponent } from './vista-detallada/vista-detallada.component';
 import { RouterModule } from '@angular/router';
 import { BuscarAnimalesComponent } from './buscar-animales/buscar-animales.component';
-=======
 import { CategoriasComponent } from './categorias/categorias.component';
->>>>>>> Stashed changes
 
 
 @NgModule({
   declarations: [
-<<<<<<< Updated upstream
     CatalogoComponent,
     ProductoComponent,
     BannerComponent,
     VistaDetalladaComponent,
-    BuscarAnimalesComponent
-=======
+    BuscarAnimalesComponent,
     CategoriasComponent
->>>>>>> Stashed changes
   ],
   imports: [
     CommonModule
@@ -30,7 +24,8 @@ import { CategoriasComponent } from './categorias/categorias.component';
   exports : [
     CatalogoComponent,
     BannerComponent,
-    BuscarAnimalesComponent
+    BuscarAnimalesComponent,
+    CategoriasComponent
   ]
 })
 export class EcommerceModule { }
