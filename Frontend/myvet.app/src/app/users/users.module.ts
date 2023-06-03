0768336198e4cebb08d7e7b31f4484b0b75f163e
Santiago Peironi <santiagopeironi@gmail.com>
2023-06-03 20:56:05 -0300

    agregué componente menú en el módulo users

--- Frontend/myvet.app/src/app/users/users.module.ts
+++ Frontend/myvet.app/src/app/users/users.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PerfilComponent } from './perfil/perfil.component';
+import { MenuComponent } from './menu/menu.component';
 
 
 
 @NgModule({
   declarations: [
-    PerfilComponent
+    PerfilComponent,
+    MenuComponent
   ],
   imports: [
     CommonModule
