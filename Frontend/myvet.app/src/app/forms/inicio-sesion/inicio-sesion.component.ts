cc8556b2754d34cf2cfc5ffcdf433f7a24457fff
Florencia <riosflorenciaitati@gmail.com>
2023-06-04 21:01:08 -0300

    Formulario de Login y validaciones

--- Frontend/myvet.app/src/app/forms/inicio-sesion/inicio-sesion.component.ts
+++ Frontend/myvet.app/src/app/forms/inicio-sesion/inicio-sesion.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-inicio-sesion',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./inicio-sesion.component.css']
 })
 export class InicioSesionComponent {
+  formLogin = this.fb.group({
+    email: ['',[Validators.required, Validators.email]],
+    password: ['', Validators.required]
+  })
+
+  invalido=false;
+  enviado=false;
+
+  constructor(private fb: FormBuilder) {}
+
+  mostrarError(nombre:string, error:string){
+    return this.formLogin.get(nombre)?.hasError(error) &&
+      ( this.formLogin.get(nombre)?.touched || this.enviado)
+    }
+
+    onSubmit() {
+      this.enviado = true
+  
+      if(this.formLogin.invalid) {
+        this.invalido = true
+        return
+      }
+      else {
+        this.invalido = false
+        console.log(this.formLogin.value) 
+        // INYECTAR SERVICIO CON MÉTODO GET
+      }
+    }
+
+
 
 }
+
+
