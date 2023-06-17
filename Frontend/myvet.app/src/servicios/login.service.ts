4cc4de0b103b687a81fb9411731bb2cd3f28a8c3
Lorena Aylen <lag9512@gmail.com>
2023-06-16 22:11:48 -0300

    Implementé el servicio para hacer login. La request funciona correctamente

--- Frontend/myvet.app/src/servicios/login.service.ts
+++ Frontend/myvet.app/src/servicios/login.service.ts
@@ -1,27 +1,23 @@
 import { Injectable } from '@angular/core';
-/* import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs'; */
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  isLogged: boolean = false;
+  userToken: string = '';
+
+  constructor(private http: HttpClient) { }
+
+  loginUser(user:any): Observable<any>{
+    return this.http.post("http://localhost:8000/api/login/", user);
+  }
+
+  logout(token: string) {
+    console.log(token)
+  }
 }
-
-/* 
-
-- crear función que contenga método GET para
-traer los usuarios 
-- hacer una segunda función que valide que el user
-que se ingrese al form de log in existe,
-- si existe tiene que comprobar que la contraseña sea
-la misma que se ingresa en el formulario, que la que 
-trae el GET
-- si el usuario no existe, tiene que emitirse un mensaje
-de error que diga que el user no es válido:
-emitir un mensaje que diga que el usuario y/o contraseña
-no es válido, y ponga los campos en rojo
-
-*/
