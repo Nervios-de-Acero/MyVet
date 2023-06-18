b8c781d97bceec64a21e4a28d16fd10e01b2c773
Yanina <unrein.syanina@gamil.com>
2023-06-18 13:55:17 -0300

    arreglando responsivo del navbar y footer de las otras paginas, funcion llamar

--- Frontend/myvet.app/src/app/shared/footer/footer.component.ts
+++ Frontend/myvet.app/src/app/shared/footer/footer.component.ts
@@ -7,5 +7,8 @@ import { Router } from '@angular/router';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
-
+  llamarTelefono() {
+    const numeroTelefono = '+54113456789'; // Aquí debes proporcionar el número de teléfono que deseas llamar
+    window.open(`tel:${numeroTelefono}`);
+  }
 }
