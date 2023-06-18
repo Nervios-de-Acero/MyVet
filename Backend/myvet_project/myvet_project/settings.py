8a90be864e89ea21cf4e0c9f073375cc6d5a07a1
Yanina <unrein.syanina@gamil.com>
2023-06-18 02:22:52 -0300

    arreglos de conflictos

--- Backend/myvet_project/myvet_project/settings.py
+++ Backend/myvet_project/myvet_project/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'myvet_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'myvetproyect',
+        'NAME': 'myvet',
         'USER': 'root',
         'PASSWORD': 'root',
         'HOST': 'localhost',
